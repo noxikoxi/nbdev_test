0b9339425918705f520df9e0e7c150b2dd4f210e
noxikoxi <noxikoxi@gmail.com>
2024-03-07 14:59:16 +0100

    first test function

--- nbdev_test/_modidx.py
+++ nbdev_test/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://noxikoxi.github.io',
                 'git_url': 'https://github.com/noxikoxi/nbdev_test',
                 'lib_path': 'nbdev_test'},
-  'syms': {'nbdev_test.core': {'nbdev_test.core.foo': ('core.html#foo', 'nbdev_test/core.py')}}}
+  'syms': { 'nbdev_test.core': { 'nbdev_test.core.foo': ('core.html#foo', 'nbdev_test/core.py'),
+                                 'nbdev_test.core.say_hello': ('core.html#say_hello', 'nbdev_test/core.py')}}}
